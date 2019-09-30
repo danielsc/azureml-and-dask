1fdf155f39679f0c219d6ae8709de4a2bf7b9c87
Ubuntu <danielsc@vnettestvm.qfqcxvt3lxlubldh0nzxxrnd3h.ax.internal.cloudapp.net>
2019-09-30 18:15:23 +0000

    documenation

--- rapids_interactive/dask/jupyter-preload.py
+++ rapids_interactive/dask/jupyter-preload.py
@@ -7,4 +7,7 @@ def dask_setup(scheduler):
     ip = socket.gethostbyname(socket.gethostname())
     app.ip="0.0.0.0"
     app.initialize([])
-    Run.get_context().log("jupyter-server", "http://" + ip + ":" + str(app.port) + "/?token=" + app.token)
+    Run.get_context().log("jupyter-url", "http://" + ip + ":" + str(app.port) + "/?token=" + app.token)
+    Run.get_context().log("jupyter-port", app.port)
+    Run.get_context().log("jupyter-token", app.token)
+    Run.get_context().log("jupyter-ip", ip)
